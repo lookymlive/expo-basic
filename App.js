799acc395e327fd24c62ba63a0113937647ac021
luisdtv <luisdtv@gmail.comL>
2025-02-11 20:52:16 -0300

    Add new dependencies and update App component layout

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="light" />
+      <Image  source={require('./assets/lookym.png')} 
+      style={{ 
+        width: 305, 
+        height: 159,
+        }} />
+      
+      <Text style={{color: 'white'}}>Welcome to Lookym! </Text>
+       <Text>
+        <h1>Lookym</h1></Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +22,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
   },
